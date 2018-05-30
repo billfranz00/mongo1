729c71adabf3100ed6190e708cb62c56ed1b0bc0
billyf <Maggie91>
2018-05-30 14:22:16 -0700

    Reading Data

--- app.js
+++ app.js
@@ -18,23 +18,68 @@ mongoose
 
 const { Instructor } = require("./models");
 
-// const billy = new Instructor({ firstName: "Billy"});
-// billy
-// 	.save()
-// 	.then(newBilly => {
-// 		console.log(newBilly);
-// 	})
-// 	.catch(err => {
-// 		console.log("ERror saving...", err);
-// 	});
+// CRUD Operations
 
-const billy = Instructor.create({ firstName: "Billy" })
-	.then(newInst => {
-		console.log(newInst);
+// Create
+const billy = new Instructor({ firstName: "Billy"});
+billy
+	.save()
+	.then(newBilly => {
+		console.log(newBilly);
 	})
 	.catch(err => {
-		console.log("Error creating", err);
+		console.log("ERror saving...", err);
 	});
+
+// const billy = Instructor.create({ firstName: "Billy", lastName: "Franz" })
+// 	.then(newInst => {
+// 		console.log(newInst);
+// 	})
+// 	.catch(err => {
+// 		console.log("Error creating", err);
+// 	});
+
+// Read
+// Instructor.find({}) // Find multiple
+// 	.then(instructors => {
+// 		console.log(instructors);
+// 	})
+// 	.catch(err => {
+// 		console.log("error finding!", err);
+// 	});
+
+// Instructor.findOne({firstName: 'Billy'}) // Find single record
+// 	.then(inst => {
+// 		console.log(inst);
+// 	})
+// 	.catch(err => {
+// 		console.log("error finding one!", err);
+// 	});
+
+// Instructor.findById(2) // Find by ID (useful with req.params)
+// 	.then(inst => {
+// 		console.log(inst);
+// 	})
+// 	.catch(err => {
+// 		console.log("error finding by ID!", err);
+// 	});
+
+const query = Instructor.findOne({firstName: "Billy"}); // Query Builder Syntax
+
+query.select("firstName lastName"); // 
+
+query
+	.exec()
+	.then(inst => {
+		console.log(inst);
+		console.log('%s has the last name "%s".', inst.firstName, inst.lastName);
+	})
+	.catch(err => {
+		console.log("ERROR with querying", err);
+	});
+
+// Update
+
 
 app.listen(3000, () => {
 	console.log('Bro, you connected, head over to localhost:3000...');
